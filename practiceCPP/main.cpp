e15a8c75d1f16d4ac54235a0a50e038ebcd9e847
swapnil <bhalerao.swapnil11@gmail.com>
2021-09-13 08:35:39 +0530

    Initial Commit

---
+++ practiceCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practiceCPP
+//
+//  Created by Swapnil Bhalerao on 13/09/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
